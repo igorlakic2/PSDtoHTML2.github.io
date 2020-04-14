56e787f5efbc3d85c862a6ab36a72aa09bfb171f
igorlakic2 <igor.lakic2@gmail.com>
2020-04-14 23:43:07 +0200

    sass test

---
+++ script.js
@@ -0,0 +1,18 @@
+let img = document.querySelector(".right");
+let left1 = document.querySelector(".left1");
+let left2 = document.querySelector(".left2");
+let left3 = document.querySelector(".btns");
+
+window.onload = () => {
+    img.style.opacity = 1;
+    img.style.right = '40px';
+
+    left1.style.opacity = 1;
+    left1.style.marginLeft = 0;
+
+    left2.style.opacity = 1;
+    left2.style.marginLeft = 0;
+
+    left3.style.opacity = 1;
+    left3.style.marginLeft = 0;
+}
